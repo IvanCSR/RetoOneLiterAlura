da2beedf2fcbf3f18de489024c6bb383327e11b7
IvanCSR <ivancsr@outlook.com>
2025-01-05 16:01:25 -0500

    primera version del proyecto

---
+++ src/test/java/com/desafioLibros/Literalura/LiteraluraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafioLibros.Literalura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiteraluraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
